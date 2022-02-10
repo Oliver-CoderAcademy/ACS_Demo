82be33b0cf7500ab9e2e483dd964e7c96f2bd7c1
Oliver-CoderAcademy <oliver.lavers@coderacademy.edu.au>
2022-02-10 15:25:57 +1000

    test

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -124,4 +124,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
+PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__), 'demo')
